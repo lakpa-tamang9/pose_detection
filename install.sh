2c080caaad596c8815cde4a4bab9c6e43f9e7a8e
lakpa-tamang9 <Ld.tamang25@gmail.com>
2022-06-13 15:42:29 +0900

    add download models via bash

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#singlepose thunder
+wget https://tfhub.dev/google/lite-model/movenet/singlepose/thunder/3?lite-format=tflite
+
+# singlepose lightning
+wget https://tfhub.dev/google/lite-model/movenet/singlepose/lightning/3?lite-format=tflite
